6d68b3415dee9603207933394606b795723c8685
MAshiroro <45526280+Mashiroro@users.noreply.github.com>
2021-09-09 00:04:20 +0800

    SMB setup
    creates a folder in C drive and allows anonymous logon for domain controller.

---
+++ Powershell/SMB.ps1
@@ -0,0 +1,19 @@
+﻿function VulnAD-PublicSMBShare {
+<#Create common folder#>
+mkdir C:\Common
+<#Create SMB share#>
+New-SmbShare -Name Common -Path C:\Common -FullAccess Everyone
+<#enable guest account#>
+Enable-LocalUser -Name "Guest"
+
+<#set ACL rule to add guest account to permissions giving full access#>
+$acl = Get-Acl C:\Common
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Guest","FullControl","Allow")
+$acl.SetAccessRule($AccessRule)
+$acl | Set-Acl C:\Common
+
+<#edit registry to enable everyoneincludesanonymous for gpo #>
+Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa' -Name 'EveryoneIncludesAnonymous' -value '1'
+<#create new reigstry to allow C:\Common for anonymous logon  #>
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters' -name "NullSessionShares" -PropertyType MultiString -value "C:\Common"
+} 
