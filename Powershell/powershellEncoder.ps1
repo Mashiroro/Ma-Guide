e3b725e221f9bdb2f04304ce54f1209e038300d9
MAshiroro <45526280+Mashiroro@users.noreply.github.com>
2022-07-10 18:49:22 +0800

    powershell encoder and decoder

---
+++ Powershell/powershellEncoder.ps1
@@ -0,0 +1,5 @@
+﻿$Command =  "echo hello"
+$Encoded = [convert]::ToBase64String([System.Text.encoding]::Unicode.GetBytes($command)) 
+Write-Output $Encoded
+
+powershell.exe -EncodedCommand $Encoded
